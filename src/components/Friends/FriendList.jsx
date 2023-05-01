7ec1ff80a2c749a5ff2cfdf91545ba07dbae1c66
Karinkarllina <karinkhaliullina@gmail.com>
2023-05-01 23:49:00 +0300

    Продовження css

--- src/components/Friends/FriendList.jsx
+++ src/components/Friends/FriendList.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types'; 
 import { FriendListItem } from './FriendListItem';
+import css from './friends.module.css';
 
 export const FriendList = ({ friends }) => {
     return (
-        <ul className="friend-list">
+        <ul className={css.friendList}>
             {friends.map((friend) => (
                 <FriendListItem
                     key={friend.id}
--- src/components/Transaction/TransactionHistory.jsx
+++ src/components/Transaction/TransactionHistory.jsx
@@ -1,17 +1,18 @@
 import PropTypes from 'prop-types';
+import css from './Transaction.module.css';
 
 export const TransactionHistory = ({ items }) => {
     return ( 
-        <table className="transaction-history">
-            <thead>
-                <tr>
-                <th>Type</th>
-                <th>Amount</th>
-                <th>Currency</th>
+        <table className={css.transactionHistory}>
+            <thead className={css.transactionTableHead}>
+                <tr className={css.tableHeadList}>
+                <th className={css.tableHeadItem}>Type</th>
+                <th className={css.tableHeadItem}>Amount</th>
+                <th className={css.tableHeadItem}>Currency</th>
                 </tr>
             </thead>
 
-            <tbody>
+            <tbody className={css.transactionTableBody}>
                 {items.map((item) => (
                     <TableBody
                         key={item.id}
@@ -31,10 +32,10 @@ export const TransactionHistory = ({ items }) => {
 
 const TableBody = ({ id, type, amount, currency }) => {
     return (
-        <tr id={id}>
-            <td>{type}</td>
-            <td>{amount}</td>
-            <td>{currency}</td>
+        <tr id={id} className={css.tableBodyList}>
+            <td className={css.tableBodyItem}>{type}</td>
+            <td className={css.tableBodyItem}>{amount}</td>
+            <td className={css.tableBodyItem}>{currency}</td>
         </tr>
   );
 };
