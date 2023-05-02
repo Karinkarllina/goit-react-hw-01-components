80057049298eec43c1f45475675b01f05e4a2775
Karinkarllina <karinkhaliullina@gmail.com>
2023-05-02 14:39:57 +0300

    Дороблено css

--- src/components/Friends/FriendListItem.jsx
+++ src/components/Friends/FriendListItem.jsx
@@ -2,12 +2,20 @@ import PropTypes from 'prop-types';
 import css from './friends.module.css';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => {
+    function typeStatus() {
+        if (isOnline === true ) {
+            return `${css.status} ${css.isOnline}`
+        } else {
+            return `${css.status} ${css.isOffline}`
+        }
+    }
     return (
             <li className={css.item}>
-                <span className={css.status}>{isOnline }</span>
+                <span className={typeStatus()}>{isOnline }</span>
                 <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
                 <p className={css.name}>{name}</p>
-            </li>
+        </li>
+
     )
 }
 
@@ -17,3 +25,5 @@ FriendListItem.propTypes = {
     name: PropTypes.string.isRequired,
     isOnline: PropTypes.bool.isRequired,
 }
+
+
